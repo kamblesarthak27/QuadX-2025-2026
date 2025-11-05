2d317213d35f2b793ab8a877f1ce1746a921be9f
AmalMadapaty <140457148+AmalMadapaty@users.noreply.github.com>
2025-11-05 10:00:31 -0600

    Testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpShooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpShooter.java
@@ -4,12 +4,11 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="TeleOp Shooter", group="TeleOp")
-public class TeleOpshooter extends OpMode {
+public class TeleOpShooter extends OpMode {
 
     private ElapsedTime time = new ElapsedTime();
     private DcMotorEx outtake;
